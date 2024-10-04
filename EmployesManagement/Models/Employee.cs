51cc14cffcebbc770489488c9409a9f5240fbdcd
Phillipe Sobrinho <phillipe_sobrinho@hotmail.com>
2024-10-04 22:20:14 +0100

    Adicionar arquivos de projeto.

---
+++ EmployesManagement/Models/Employee.cs
@@ -0,0 +1,32 @@
+﻿namespace EmployesManagement.Models
+{
+    public class Employee: UserActivity
+    {
+        public int Id { get; set; }
+
+        public string EmployeeNumber { get; set; }
+
+        public string FirstName { get; set; }
+
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName => $"{FirstName} {MiddleName}  {LastName}";
+
+        public string PhoneNumber { get; set; }
+
+        public string EmailAddress{ get; set; }
+
+        public string Country { get; set; }
+
+        public string DataOfBirth{ get; set; }
+
+        public string Address { get; set; }
+
+        public string Departament { get; set; }
+
+
+    }
+}
---
+++ EmployesManagement/Models/UserActivity.cs
@@ -0,0 +1,15 @@
+﻿namespace EmployesManagement.Models
+{
+    public class UserActivity
+    {
+
+        public String? CreateById { get; set; }
+
+        public DateTime CreateOn{ get; set; }
+
+
+        public String? ModifyById { get; set; }
+
+        public DateTime ModifiedOn { get; set; }
+    }
+}
